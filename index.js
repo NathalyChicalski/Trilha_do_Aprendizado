d520637f107366829df81cc95d7c1e60baa42a55
NathalyChicalski <chicalski.nathaly@gmail.com>
2022-12-04 16:50:01 -0300

    version 4.0
    adição da imagem e alinhamento das divs;
    site finalizado.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const bnt = document.querySelector('#bnt') 
 bnt.addEventListener('click', () => {  /*sempre executa uma ação q mandar, nesse caso é click*/
-window.scroll({top: window.innerHeight, behavior: "smooth"})
+window.scroll({top: window.innerHeight, behavior: "smooth"})/*descer suavemente*/
 })
